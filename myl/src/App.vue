01218be31cb33fafaf557e0fb4fa1ceb828131c3
novembergaran <yao_li@yurenedugroup.com>
2017-05-27 18:09:30 +0800

    1

--- myl/src/App.vue
+++ myl/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
 
-
+<img class="pic" src="../src/assets/1.jpeg">
+<img class="pic" src="../src/assets/2.jpeg">
+<img class="pic" src="../src/assets/3.jpeg">
+<img class="pic" src="../src/assets/4.jpeg">
+<img class="pic" src="../src/assets/5.jpeg">
   </div>
 </template>
 
@@ -11,6 +15,10 @@ export default {
 }
 </script>
 
-<style>
+<style >
+  @font:STXihei, "华文细黑", "Microsoft YaHei", "微软雅黑";
 
+  pic{
+
+  }
 </style>
